a1f71957bf3fab23a1e943e88ee3bb1c86886ed5
Aaron Brice <aaron.brice@gmail.com>
2025-01-20 18:21:55 -0700

    Initial disk access, does not mount

--- src/main.c
+++ src/main.c
@@ -4,45 +4,138 @@
  * SPDX-License-Identifier: Apache-2.0
  */
 
-#include <stdio.h>
 #include <zephyr/kernel.h>
-#include <zephyr/drivers/gpio.h>
+#include <zephyr/device.h>
+#include <zephyr/storage/disk_access.h>
+#include <zephyr/logging/log.h>
+#include <zephyr/fs/fs.h>
 
-/* 1000 msec = 1 sec */
-#define SLEEP_TIME_MS   1000
+#define DISK_DRIVE_NAME "SD"
+#define DISK_MOUNT_PT "/"DISK_DRIVE_NAME":"
+#define FS_RET_OK FR_OK
 
-/* The devicetree node identifier for the "led0" alias. */
-#define LED0_NODE DT_ALIAS(led0)
+#include <ff.h>
 
-/*
- * A build error on this line means your board is unsupported.
- * See the sample documentation for information on how to fix this.
+static FATFS fat_fs;
+/* mounting info */
+static struct fs_mount_t mp = {
+	.type = FS_FATFS,
+	.fs_data = &fat_fs,
+    .flags = FS_MOUNT_FLAG_READ_ONLY | FS_MOUNT_FLAG_NO_FORMAT,
+};
+
+#define FS_RET_OK FR_OK
+
+LOG_MODULE_REGISTER(main);
+
+static const char *disk_mount_pt = DISK_MOUNT_PT;
+
+/* List dir entry by path
+ *
+ * @param path Absolute path to list
+ *
+ * @return Negative errno code on error, number of listed entries on
+ *         success.
  */
-static const struct gpio_dt_spec led = GPIO_DT_SPEC_GET(LED0_NODE, gpios);
+static int lsdir(const char *path)
+{
+	int res;
+	struct fs_dir_t dirp;
+	static struct fs_dirent entry;
+	int count = 0;
+
+	fs_dir_t_init(&dirp);
+
+	/* Verify fs_opendir() */
+	res = fs_opendir(&dirp, path);
+	if (res) {
+		LOG_ERR("Error opening dir %s [%d]", path, res);
+		return res;
+	}
+
+	LOG_INF("\nListing dir %s ...", path);
+	for (;;) {
+		/* Verify fs_readdir() */
+		res = fs_readdir(&dirp, &entry);
+
+		/* entry.name[0] == 0 means end-of-dir */
+		if (res || entry.name[0] == 0) {
+			break;
+		}
+
+		if (entry.type == FS_DIR_ENTRY_DIR) {
+			LOG_INF("[DIR ] %s", entry.name);
+		} else {
+			LOG_INF("[FILE] %s (size = %zu)",
+				entry.name, entry.size);
+		}
+		count++;
+	}
+
+	/* Verify fs_closedir() */
+	fs_closedir(&dirp);
+	if (res == 0) {
+		res = count;
+	}
+
+	return res;
+}
 
 int main(void)
 {
-	int ret;
-	bool led_state = true;
+	/* raw disk i/o */
+	do {
+		static const char *disk_pdrv = DISK_DRIVE_NAME;
+		uint64_t memory_size_mb;
+		uint32_t block_count;
+		uint32_t block_size;
 
-	if (!gpio_is_ready_dt(&led)) {
-		return 0;
-	}
-
-	ret = gpio_pin_configure_dt(&led, GPIO_OUTPUT_ACTIVE);
-	if (ret < 0) {
-		return 0;
-	}
-
-	while (1) {
-		ret = gpio_pin_toggle_dt(&led);
-		if (ret < 0) {
-			return 0;
+		if (disk_access_ioctl(disk_pdrv,
+				DISK_IOCTL_CTRL_INIT, NULL) != 0) {
+			LOG_ERR("Storage init ERROR!");
+			break;
 		}
 
-		led_state = !led_state;
-		printf("LED state: %s\n", led_state ? "ON" : "OFF");
-		k_msleep(SLEEP_TIME_MS);
+		if (disk_access_ioctl(disk_pdrv,
+				DISK_IOCTL_GET_SECTOR_COUNT, &block_count)) {
+			LOG_ERR("Unable to get sector count");
+			break;
+		}
+		LOG_INF("Block count %u", block_count);
+
+		if (disk_access_ioctl(disk_pdrv,
+				DISK_IOCTL_GET_SECTOR_SIZE, &block_size)) {
+			LOG_ERR("Unable to get sector size");
+			break;
+		}
+		LOG_INF("Sector size %u", block_size);
+
+		memory_size_mb = (uint64_t)block_count * block_size;
+		LOG_INF("Memory Size(MB) %u", (uint32_t)(memory_size_mb >> 20));
+
+		if (disk_access_ioctl(disk_pdrv,
+				DISK_IOCTL_CTRL_DEINIT, NULL) != 0) {
+			LOG_ERR("Storage deinit ERROR!");
+			break;
+		}
+	} while (0);
+
+	mp.mnt_point = disk_mount_pt;
+
+	int res = fs_mount(&mp);
+
+	if (res == FS_RET_OK) {
+		LOG_INF("Disk mounted.");
+		lsdir(disk_mount_pt);
+	} else {
+		LOG_ERR("Error mounting disk.");
 	}
+
+	fs_unmount(&mp);
+
+	while (1) {
+		k_sleep(K_MSEC(1000));
+	}
+
 	return 0;
 }
